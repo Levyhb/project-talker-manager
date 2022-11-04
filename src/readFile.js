9dc3ddd15535a0df547a9c647e1eb740f6bd793b
levyhb <levybholanda@gmail.com>
2022-11-04 17:10:53 -0300

    Requisito 02

--- src/readFile.js
+++ src/readFile.js
@@ -4,8 +4,8 @@ const path = require('path');
 const pathFile = path.resolve(__dirname, 'talker.json');
 
 const readAllFiles = async () => {
-  const talkerData = await readFile(pathFile, { encoding: 'utf-8' });
-  return JSON.parse(talkerData);
+  const response = await readFile(pathFile, { encoding: 'utf-8' });
+  return JSON.parse(response);
 };
 
 module.exports = { 
--- src/routes/talkerRoutes.js
+++ src/routes/talkerRoutes.js
@@ -9,4 +9,18 @@ router.get('/', async (_req, res) => {
   return res.status(200).json(response);
 });
 
+router.get('/:id', async (req, res) => {
+  const response = await readAllFiles();
+  const id = Number(req.params.id);
+  const talker = response.find((t) => t.id === id);
+  console.log(talker);
+  if (talker) {
+    res.status(200).json(talker);
+  } else {
+    res.status(404).json({
+      message: 'Pessoa palestrante não encontrada',
+    });
+  }
+});
+
 module.exports = router;
