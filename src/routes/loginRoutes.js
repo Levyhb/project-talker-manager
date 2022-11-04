bf5d216b360a0ddb585a1a02d52782af77c41cde
levyhb <levybholanda@gmail.com>
2022-11-04 18:00:42 -0300

    Requisito 04

--- src/routes/loginRoutes.js
+++ src/routes/loginRoutes.js
@@ -6,7 +6,18 @@ const router = express.Router();
 
 const generateToken = () => crypto.randomBytes(8).toString('hex');
 
-router.post('/', (__req, res) => {
+router.post('/', (req, res) => {
+  const { email, password } = req.body;
+  const emailRegex = /\S+@\S+\.\S+/.test(email);
+  if (!email) {
+    res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  } if (!emailRegex) {
+    res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  } if (!password) {
+    res.status(400).json({ message: 'O campo "password" é obrigatório' });
+  } if (password.length < 6) {
+    res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
   const token = generateToken();
   res.status(200).json({ token });
 });
